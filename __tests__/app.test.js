26a87b026bd08f269ab10a397c421856133c301a
Paul Brubaker <pbrubak@gmail.com>
2021-10-10 23:15:10 -0700

    passing test for GET /me route

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,6 +2,7 @@ const pool = require('../lib/utils/pool.js');
 const setup = require('../data/setup.js');
 const request = require('supertest');
 const app = require('../lib/app.js');
+const UserService = require('../lib/services/UserService.js');
 
 describe('authentication-ctbe routes', () => {
     beforeEach(() => {
@@ -41,6 +42,34 @@ describe('authentication-ctbe routes', () => {
         expect(res.body).toEqual({
             email: 'cow@moo.com',
             id: expect.any(String),
+        });
+    });
+
+    it('should return 401 for trying to login with bad credentials', async () => {
+        await UserService.create({ email: 'cow@moo.com', password: 'mooo' });
+        const res = await request(app)
+            .post('/api/auth/signin')
+            .send({ email: 'cow@moo.com', password: 'moooo' });
+        expect(res.statusCode).toEqual(401);
+        expect(res.body.message).toEqual('Invalid email/password combination');
+    });
+
+    it('GET /me should respond with the currently logged in user', async () => {
+        const user = await UserService.create({
+            email: 'cow@moo.com',
+            password: 'mooo',
+        });
+
+        const agent = request.agent(app);
+        await agent
+            .post('/api/auth/signin')
+            .send({ email: 'cow@moo.com', password: 'mooo' });
+
+        const res = await agent.get('/api/auth/me');
+
+        expect(res.body).toEqual({
+            id: user.id,
+            email: 'cow@moo.com',
         });
     });
 
