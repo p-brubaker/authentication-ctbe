e9d6add257af6ec4b5d0986b02a0d4c31915fb73
Paul Brubaker <pbrubak@gmail.com>
2021-10-10 18:20:32 -0700

    failing test for POST new user route

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -4,11 +4,22 @@ const request = require('supertest');
 const app = require('../lib/app.js');
 
 describe('authentication-ctbe routes', () => {
-  beforeEach(() => {
-    return setup(pool);
-  });
+    beforeEach(() => {
+        return setup(pool);
+    });
 
-  afterAll(() => {
-    pool.end();
-  });
+    it('should return a newly POSTed User id and email', async () => {
+        const res = await request(app)
+            .post('api/auth/signup')
+            .send({ email: 'cow@moo.com', password: 'mooo' });
+
+        expect(res).toEqual({
+            id: expect.any(String),
+            email: 'cow@moo.com',
+        });
+    });
+
+    afterAll(() => {
+        pool.end();
+    });
 });
